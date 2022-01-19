6ef241d4141f878ea0766e1ba981924913714bce
orsoxii <orsooo@mailforkids.net>
2022-01-19 13:10:21 +0100

    yu

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
+
+app.listen(3000)
+
+app.set("view-engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {name: "orso"})
+})
